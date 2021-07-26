0dd33db8e3b2e92dcebac7da914e63f6f809293f
charitreddy <charitreddy@gmail.com>
2021-07-26 21:00:24 +0530

    Structure setup

---
+++ src/components/personal-info/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './personal-info.css';
+import Divider from '../divider';
+const PersonalInfo = () => {
+  return (
+    <section className="personal-info">
+      <Divider />
+    </section>
+  );
+};
+
+export default PersonalInfo;
---
+++ src/components/professional-experience/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProfessionalExperience = () => {
+  return <div></div>;
+};
+
+export default ProfessionalExperience;
